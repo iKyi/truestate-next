f08bceba2252f7621f17c351062c8937a5e4d7e2
iKyi <ikymetaverse@gmail.com>
2022-11-10 01:04:35 +0200

    changed thing

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,19 +1,14 @@
-import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import { NextPage } from "next";
 import LayoutWrapper from "../components/Reusable/Layout/LayoutWrapper";
 // import ContactBox from "../components/Reusable/ContactBox/ContactBox";
 // import LayoutWrapper from "../components/Reusable/Layout/LayoutWrapper";
 // import PageHeader from "../components/Reusable/PageHeader";
-import { fetchAPI } from "../lib/api";
 
-type PatruSutePatruProps = {
-  main: Record<any, any>;
-  services: any[];
-};
+type PatruSutePatruProps = {};
 
-const PatruSutePatru: NextPage<PatruSutePatruProps> = ({ main, services }) => {
-  const { seo, pageHeader, headerImage } = main ?? {};
+const PatruSutePatru: NextPage<PatruSutePatruProps> = () => {
+  // const { seo, pageHeader, headerImage } = main ?? {};
   return (
     <LayoutWrapper seo={{}}>
       <Box>Page not found</Box>
@@ -25,16 +20,16 @@ const PatruSutePatru: NextPage<PatruSutePatruProps> = ({ main, services }) => {
   //   <ContactBox section />
   // </LayoutWrapper>
 };
-export async function getStaticProps() {
-  // Run API calls in parallel
-  //   const [main, services] = await Promise.all([
-  //     fetchAPI("/PatruSutePatru?populate=*"),
-  //     fetchAPI("/services?populate=*"),
-  //   ]);
-  return {
-    props: { main: null, services: null },
-    revalidate: 60,
-  };
-}
+// export async function getStaticProps() {
+//   // Run API calls in parallel
+//   //   const [main, services] = await Promise.all([
+//   //     fetchAPI("/PatruSutePatru?populate=*"),
+//   //     fetchAPI("/services?populate=*"),
+//   //   ]);
+//   return {
+//     props: { main: null, services: null },
+//     revalidate: 60,
+//   };
+// }
 
 export default PatruSutePatru;
